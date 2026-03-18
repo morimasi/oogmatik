2559dc6415aa2dcd60ae822c06538e65583d90f6
morimasi <morimasi@example.com>
2026-03-18 15:09:30 +0300

    fix: resolve vite alias for vercel build

--- oogmatik/vite.config.ts
+++ oogmatik/vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react';
 
+import { fileURLToPath, URL } from 'node:url';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
   define: {
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
     'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
